694ca3d12b5fd2a8f6af4fefa9d781a212bec12f
whynotcrybot <alexanderkatsero@gmail.com>
2018-01-28 18:51:40 +0200

    remove dotenv

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,28 +1,3 @@
-require('dotenv').config()
-
-// for further usage
-const devConfig = {}
-
-const testConfig = {}
-
-const prodConfig = {}
-
-const defaultConfig = {
-  PORT: process.env.PORT || 3030
-}
-
-function envConfig (env) {
-  switch (env) {
-    case 'development':
-      return devConfig
-    case 'test':
-      return testConfig
-    default:
-      return prodConfig
-  }
-}
-
 module.exports = {
-  ...defaultConfig,
-  ...envConfig(process.env.NODE_ENV)
+  PORT: process.env.PORT || 3030
 }
